a78afce64188eeda1a99d04df3f01a542a35c286
Rodrigo Chacon <rochacon@gmail.com>
2020-09-20 22:28:02 -0300

    helloworld: use pre>code container for request logging and issue request every 1s

--- net/grpc/gateway/examples/helloworld/client.js
+++ net/grpc/gateway/examples/helloworld/client.js
@@ -16,37 +16,24 @@
  *
  */
 
-const {HelloRequest, RepeatHelloRequest,
-       HelloReply} = require('./helloworld_pb.js');
+const {HelloRequest, HelloReply} = require('./helloworld_pb.js');
 const {GreeterClient} = require('./helloworld_grpc_web_pb.js');
 
-var client = new GreeterClient('http://' + window.location.hostname + ':8080',
-                               null, null);
+var url = 'http://' + window.location.hostname + ':8080';
+var client = new GreeterClient(url, null, null);
+console.log("connecting to "+ url);
 
-// simple unary call
-var request = new HelloRequest();
-request.setName('World');
-
-client.sayHello(request, {}, (err, response) => {
-  if (err) {
-    console.log(`Unexpected error for sayHello: code = ${err.code}` +
-                `, message = "${err.message}"`);
-  } else {
-    console.log(response.getMessage());
-  }
-});
-
-
-// server streaming call
-var streamRequest = new RepeatHelloRequest();
-streamRequest.setName('World');
-streamRequest.setCount(5);
-
-var stream = client.sayRepeatHello(streamRequest, {});
-stream.on('data', (response) => {
-  console.log(response.getMessage());
-});
-stream.on('error', (err) => {
-  console.log(`Unexpected stream error: code = ${err.code}` +
-              `, message = "${err.message}"`);
-});
+setInterval(function() {
+  var output = document.querySelector("body>pre>code");
+  var request = new HelloRequest();
+  request.setName('grpc-web');
+  client.sayHello(request, {}, (err, response) => {
+    if (err) {
+      output.innerText += (`unexpected error for sayHello: code = ${err.code} message = "${err.message}"`);
+    } else {
+      var now = new Date().toISOString();
+      output.innerText += (now +": "+ response.getMessage());
+    }
+    output.innerText += "\n";
+  });
+}, 1000);
